4650444a6e08c3fdd1f9bd6a217f9434f81624ad
nir-programmer <niritzhak10@gmail.com>
2022-04-12 09:22:58 +0300

    MyEbuyBackEndApi Project created

---
+++ MyEbuyBackEndAPI/src/main/java/org/nir/myebuy/MyEbuyBackEndApiApplication.java
@@ -0,0 +1,13 @@
+package org.nir.myebuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEbuyBackEndApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEbuyBackEndApiApplication.class, args);
+	}
+
+}
