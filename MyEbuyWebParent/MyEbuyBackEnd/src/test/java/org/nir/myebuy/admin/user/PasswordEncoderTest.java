ece40c6938ca7a95ea584c46d49135cf777290c5
nir-programmer <niritzhak10@gmail.com>
2022-04-14 12:40:25 +0300

    FINISH ENCODE USER PASSWORD IN MYEBUYBACKEND

---
+++ MyEbuyWebParent/MyEbuyBackEnd/src/test/java/org/nir/myebuy/admin/user/PasswordEncoderTest.java
@@ -0,0 +1,28 @@
+package org.nir.myebuy.admin.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoderTest 
+{
+	@Test
+	public void testEncodePassword()
+	{
+		//GIVEN
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String rawPassword = "nam2020"; 
+		
+		//WHEN
+		String encodedPassword = passwordEncoder.encode(rawPassword);
+		boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
+	
+		//THEN
+		System.out.println(encodedPassword); 
+		assertThat(matches).isTrue();
+		
+	}
+
+}
