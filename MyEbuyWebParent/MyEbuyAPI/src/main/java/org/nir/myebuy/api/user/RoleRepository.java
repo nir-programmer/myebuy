5e7ecef7b9286449e4a5658ddb959388645601f0
nir-programmer <niritzhak10@gmail.com>
2022-04-15 10:37:37 +0300

    MyEbuyBackEnd CREATE WORKS and the API returns the user by id and list users

--- MyEbuyWebParent/MyEbuyAPI/src/main/java/org/nir/myebuy/api/user/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package org.nir.myebuy.api.user;
-
-import org.nir.myebuy.common.entity.Role;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RoleRepository extends CrudRepository<Role, Integer> {
-
-}
--- MyEbuyWebParent/MyEbuyAPI/src/main/java/org/nir/myebuy/api/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.nir.myebuy.api.user;
-
-import org.nir.myebuy.common.entity.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-}
