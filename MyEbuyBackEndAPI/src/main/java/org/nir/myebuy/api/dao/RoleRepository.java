48e1e464137ca44aa296ddf862a05190db7458b4
nir-programmer <niritzhak10@gmail.com>
2022-04-16 11:17:51 +0300

    OK

--- MyEbuyBackEndAPI/src/main/java/org/nir/myebuy/api/dao/RoleRepository.java
+++ MyEbuyBackEndAPI/src/main/java/org/nir/myebuy/api/dao/RoleRepository.java
@@ -11,9 +11,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
  * @author nir
  *
  */
+//http://127.0.0.1:5501
 //:4200/welcome:1 Access to XMLHttpRequest at 'http://localhost:8083/MyEbuyAdminAPI/api/users' from origin 'http://localhost:4200
 //@CrossOrigin("http://localhost:4200")
-@CrossOrigin("http://localhost:4200")
+//@CrossOrigin({"http://localhost:4200", "http://localhost:5501"})
+@CrossOrigin(origins = {"http://localhost:4200", "http://127.0.0.1:5501"})
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 
 }
--- MyEbuyBackEndAPI/src/main/java/org/nir/myebuy/api/dao/UserRepository.java
+++ MyEbuyBackEndAPI/src/main/java/org/nir/myebuy/api/dao/UserRepository.java
@@ -3,7 +3,8 @@ package org.nir.myebuy.api.dao;
 import org.nir.myebuy.common.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
-@CrossOrigin("http://localhost:4200")
+//@CrossOrigin({"http://localhost:4200", "http://localhost:5501"})
+@CrossOrigin(origins = {"http://localhost:4200", "http://127.0.0.1:5501"})
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
